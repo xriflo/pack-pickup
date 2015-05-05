2e36d9401a4505ade69716b91d12e63aaaa5ff01
Bratiloveanu Florentina - Stefania <florentina.bratiloveanu@gmail.com>
2015-05-05 07:53:50 +0300

    Cacat de link

--- packpickup/app/src/main/java/com/idp/packpickup/URL.java
+++ packpickup/app/src/main/java/com/idp/packpickup/URL.java
@@ -4,7 +4,7 @@ package com.idp.packpickup;
  * Created by happyfeet on 5/5/2015.
  */
 public class URL {
-    public final static String host = "http://127.0.0.1/";
+    public final static String host = "http://192.168.15.1/";
     public final static String loginScript = host+"login_user.php";
 
 }
