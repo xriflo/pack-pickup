d4cec081dbd7e8c6cd9bdd645e0d3afda841f8bd
Bratiloveanu Florentina - Stefania <florentina.bratiloveanu@gmail.com>
2015-05-05 06:54:17 +0300

    Revert "Revert "Incercare de a face post esuata""
    This reverts commit fb9f6e73f873d175760407e15cb9a9f86f55a973.

---
+++ packpickup/app/src/main/java/com/idp/packpickup/Functions.java
@@ -0,0 +1,28 @@
+package com.idp.packpickup;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by happyfeet on 5/4/2015.
+ */
+public class Functions {
+    public static String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
