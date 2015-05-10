7c55bdd6e920027f6ee164396b853562625dc996
Mardaloescu Serban <serban.stefanita@gmail.com>
2015-05-10 22:38:16 +0300

    Added  user's choice design

--- packpickup/app/src/main/java/com/idp/packpickup/MainActivity.java
+++ packpickup/app/src/main/java/com/idp/packpickup/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+      protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
