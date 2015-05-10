4c4a89fea00ba6ff01518613d56db2ccdd018242
Mardaloescu Serban <serban.stefanita@gmail.com>
2015-05-10 21:32:18 +0300

    Modified login script

--- server/sign_in_up.php
+++ server/sign_in_up.php
@@ -10,51 +10,39 @@ define('DB_PASSWORD', "Patqv26KdUUVhmXM"); // db password (mention your db passw
 define('DB_DATABASE', "users"); // database name
 define('DB_SERVER', "localhost"); // db server
 
-class DB_CONNECT {
- 
-    // constructor
-    function __construct() {
-        // connecting to database
-        $this->connect();
-    }
- 
-    // destructor
-    function __destruct() {
-        // closing db connection
-        $this->close();
-    }
- 
-    /**
-     * Function to connect with database
-     */
+class DB_CONNECT  {
+    var $myconn;
+    var $response = array();
     function connect() {
-        // import database connection variables
-        //require_once __DIR__ . '/db_config.php';
- 
-        // Connecting to mysql database
-        $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
- 
-        // Selecing database
-        $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
- 
-        // returing connection cursor
-        return $con;
+        $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+        if (!$con) {
+            $this->response["success"] = 0;
+            $this->response["message"] = "Could not connect to database!";
+        } else {
+            $this->myconn = $con;
+            $this->response["success"] = 1;
+            $this->response["message"] = "Connection established!";
+        }
+        return $this->myconn;
     }
- 
-    /**
-     * Function to close db connection
-     */
+
     function close() {
-        // closing db connection
-        mysql_close();
+        mysqli_close($this->myconn);
+        $this->response["success"] = 1;
+        $this->response["message"] = "Connection closed!";
     }
- 
+
 }
 
 // array for JSON response
 $response = array();
+
 // check for required fields
 if(!empty($_POST['logging'])) {
+	$db2 = new DB_CONNECT();
+	$db2->connect();
+	$db = $db2->myconn;
+	
 	if(strcmp($_POST['logging'], "signup") == 0) {
 		if (!empty($_POST['username']) && !empty(urldecode($_POST['email'])) && !empty($_POST['password']) && !empty($_POST['location'])) {
 		    $username = $_POST['username'];
@@ -62,62 +50,55 @@ if(!empty($_POST['logging'])) {
 		    $password = $_POST['password'];
 		    $location = $_POST['location'];
 		    
-		    // connecting to db
-		    $db = new DB_CONNECT();
-		 
-		    $result = mysql_query("SELECT COUNT(*) as is_present FROM users WHERE (username = '$username' OR email = '$username') AND password = '$password'");
-		 	$row = mysql_fetch_assoc($result);
-		    if($row['is_present'] == 0) {
+		    
+		    $result = mysqli_query($db, "SELECT COUNT(*) as no FROM users WHERE ((username = '$username' OR email = '$username') AND password = '$password')");
+		 	$row = mysqli_fetch_assoc($result);
+
+		    if($row['no'] == 0) {
 
 			    // mysql inserting a new row
-			    $result = mysql_query("INSERT INTO users (username, email, password, location) 
-			                            VALUES('$username', '$email', '$password', '$location')");
+			    $result = mysqli_query($db, "INSERT INTO users (username, email, password, location) 
+			                            VALUES ('$username', '$email', '$password', '$location')");
 			 
-			 	$row = mysql_fetch_assoc($result);
-			    // check if row inserted or not
+			 	// check if row inserted or not
 			    if ($result) {
 			        // successfully inserted into database
 			        $response["success"] = 1;
 			        $response["message"] = "User successfully added.";
-			 
-			        // echoing JSON response
-			        echo json_encode($response);
 			    } else {
 			        // failed to insert row
 			        $response["failure"] = 0;
 			        $response["message"] = "Oops! An error occurred.";
-			 
-			        // echoing JSON response
-			        echo json_encode($response);
 			    }
 		    } else {
 		    	$response["failure"] = 0;
 			    $response["message"] = "User already exists. Please log in!";
-				echo json_encode($response);
 		    }
 		} else {
 		    // required field is missing
-		    $response["success"] = 0;
-		    $response["message"] = "Required field(s) missing";
-		    echo json_encode($_POST);
-		    // echoing JSON response
-		    echo json_encode($response);
+		    $response["failure"] = 1;
+		    $response["message"] = "Required field(s) missing";		    
 		}
 	} else if(strcmp($_POST['logging'], "signin") == 0) {
 		if (!empty($_POST['username']) && !empty($_POST['password'])) {
 			$username = $_POST['username'];
 	   		$password = $_POST['password'];
 
-	   		$db = new DB_CONNECT();
-	   		$result = mysql_query("SELECT COUNT(*) as is_present FROM users WHERE (username = '$username' OR email = '$username') AND password = '$password'");
-		 	$row = mysql_fetch_assoc($result);
+	   		$result = mysqli_query($db, "SELECT COUNT(*) as no FROM users WHERE ((username = '$username' OR email = '$username') AND password = '$password')");
+		 	$row = mysqli_fetch_assoc($result);
 
-		 	
-		    $response["success"] = 0;
-		    $response["message"] = "Ma nigga";
-			echo json_encode($response);
+		 	if($row['no'] != 0) {
+		 		$response["success"] = 1;
+		    	$response["message"] = "Successfully logged in";
+		 	} else {
+			    $response["failure"] = 0;
+			    $response["message"] = "This account does not exist";
+		 	}
+
 		}
 	}
+	mysqli_close($db);
+	echo json_encode($response);
 }
 
 ?>
